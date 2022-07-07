f9a8a1f160d4c3fb77c79b6708d8bab22436259b
sahilbabayev44 <107620978+sahilbabayev44@users.noreply.github.com>
2022-07-07 13:58:30 +0400

    MERCEDESS

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace program_MERCEDES
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("rex", "yellow", 2);
+            dog.name = "rex";
+            dog.color = "yellow";
+            dog.age = 2;
+            Console.WriteLine($"{dog.name} {dog.color} {dog.age}");
+
+            Console.WriteLine(dog.GetDetalis());
+
+        }
+    }
+}
---
+++ dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_MERCEDES
+{
+    public class Dog
+    {
+        public string name;
+        public string color;
+        public byte age;
+        
+        public Dog(string n, string c, byte a)
+        {
+            this.name = n;
+            this.color = c;
+            this.age = a;
+
+            Console.WriteLine("Custom construktor worked");
+
+
+        }
+            public string GetDetalis()
+            {
+                return $"{name} {color} {age}";
+            }
+    }
+}
